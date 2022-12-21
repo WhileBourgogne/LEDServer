717ef8023608ea9ae852c77c5c409c6eeb319c00
WhileBourgogne <105404523+WhileBourgogne@users.noreply.github.com>
2022-12-21 21:31:32 +0000

    add arguments

--- main.c
+++ main.c
@@ -74,4 +74,4 @@ int main(int argc,char **argv) {
   glutInit(&argc,argv);
   glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
   glutInitWindowSize(800,600);
-  glutInitWindowPosition(10,1
+  glutInitWindowPosition(10,10
